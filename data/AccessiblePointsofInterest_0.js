d449052665023c246743ec5dcf2bc7a3909bd7f8
AccessLab <54842392+AccessLabTeam@users.noreply.github.com>
2025-02-11 22:28:35 +0200

    Add files via upload

---
+++ data/AccessiblePointsofInterest_0.js
@@ -0,0 +1,19 @@
+var json_AccessiblePointsofInterest_0 = {
+"type": "FeatureCollection",
+"name": "AccessiblePointsofInterest_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Building": "Town Hall", "Amenities": "Accessible Parking, Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.885843064101778, 38.392231441817451 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Building": "Health Clinic", "Amenities": "Accessible Parking, Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.883049925334113, 38.393152264057647 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Building": "Library", "Amenities": "Accessible Parking, Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.885971531908591, 38.393879819016135 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Building": "Care Center for Elderly People", "Amenities": "Accessible Parking, Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.885785046382562, 38.393977258856467 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Building": "Education", "Amenities": "Accessible Parking, Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.887165039275185, 38.391375570067218 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Building": "Church", "Amenities": "Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.883174249018133, 38.393931786947327 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Building": "Restaurants", "Amenities": "Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.885536399014516, 38.391209916300312 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Building": "Κindergarten", "Amenities": "Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.886680176907504, 38.390540801144482 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Building": "Post Office", "Amenities": "Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.883887038139846, 38.392437694026164 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Building": "SuperMarket", "Amenities": "Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.884384332875928, 38.393366633690079 ] } },
+{ "type": "Feature", "properties": { "id": "11", "Building": "Cafes", "Amenities": "Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.884956221822421, 38.391645162641211 ] } },
+{ "type": "Feature", "properties": { "id": "12", "Building": "Football Pitch", "Amenities": "Accessible Parking, Accessible Entrance" }, "geometry": { "type": "Point", "coordinates": [ 23.876145816748181, 38.393905802986396 ] } }
+]
+}
